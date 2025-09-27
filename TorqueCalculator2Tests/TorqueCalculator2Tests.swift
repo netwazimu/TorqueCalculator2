5f6eb1ce63f1cedf78b02e26d7c5651b6f7dbd8a
netwazimu <38446512+netwazimu@users.noreply.github.com>
2025-09-27 13:26:24 +0300

    Add files via upload

---
+++ TorqueCalculator2Tests/TorqueCalculator2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TorqueCalculator2Tests.swift
+//  TorqueCalculator2Tests
+//
+//  Created by Brian Omondi on 27/09/2025.
+//
+
+import Testing
+@testable import TorqueCalculator2
+
+struct TorqueCalculator2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
